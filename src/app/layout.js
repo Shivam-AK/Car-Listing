b75c7a06758f4ca6ec9217c34987507fd709c170
Shivam Kumar <shivam201907@gmail.com>
2025-05-01 14:12:34 +0530

    Setup Authentication

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,12 +13,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className}`}>
-        <Header />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className}`}>
+          <Header />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,82 @@
-export default function Header() {
-  return <div>Header</div>;
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { ArrowLeft, CarFront, Heart, Layout } from "lucide-react";
+
+export default function Header({ isAdminPage = false }) {
+  const isAdmin = true;
+
+  return (
+    <header className="fixed top-0 z-50 bg-white/80 backdrop-blur-md w-full border-b">
+      <nav className="mx-auto px-4 py-2 flex items-center justify-between">
+        <Link href={isAdminPage ? "/admin" : "/"} className="flex">
+          <Image
+            src="/logo.png"
+            alt="Vahiql Logo"
+            width={200}
+            height={60}
+            className="h-12 w-auto object-contain"
+          />
+          {isAdminPage && (
+            <span className="text-xs font-extralight">admin</span>
+          )}
+        </Link>
+
+        <div className="flex items-center space-x-4">
+          {isAdminPage ? (
+            <Link href="/">
+              <Button variant="outline">
+                <ArrowLeft />
+                <span>Back to App</span>
+              </Button>
+            </Link>
+          ) : (
+            <SignedIn>
+              <Link href="/saved-cars">
+                <Button>
+                  <Heart />
+                  <span className="hidden md:inline">Saved Cars</span>
+                </Button>
+              </Link>
+
+              {!isAdmin ? (
+                <Link href="/reservations">
+                  <Button variant="outline">
+                    <CarFront />
+                    <span className="hidden md:inline">My Reservations</span>
+                  </Button>
+                </Link>
+              ) : (
+                <Link href="/admin">
+                  <Button variant="outline">
+                    <Layout />
+                    <span className="hidden md:inline">Admin Portal</span>
+                  </Button>
+                </Link>
+              )}
+            </SignedIn>
+          )}
+
+          <SignedOut>
+            {!isAdminPage && (
+              <SignInButton forceRedirectUrl="/">
+                <Button variant="outline">Login</Button>
+              </SignInButton>
+            )}
+          </SignedOut>
+
+          <SignedIn>
+            <UserButton
+              appearance={{
+                elements: {
+                  avatarBox: "w-10 h-10",
+                },
+              }}
+            />
+          </SignedIn>
+        </div>
+      </nav>
+    </header>
+  );
 }
